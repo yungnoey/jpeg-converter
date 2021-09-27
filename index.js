f7188124e0d2c8338eff356dc15f9c2db6d24c3f
yungnoey <nehayling@gmail.com>
2021-09-26 22:25:29 -0400

    updated index.js comments

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
+// imports
 const fs = require("fs");
 const path = require("path");
 
+// dec;are directory
 let directory = __dirname;
 
+// build paths
 let rawPath = path.join(directory, "/raw");
 let exportPath = path.join(directory, "/export");
 
+// start up message
 console.log("Conversion Started...");
-let i = 0;
 
+// convert files
 fs.readdir(rawPath, (err, files) => {
   files.forEach((file) => {
-    // read file
+    // build file info
     let filePath = path.join(rawPath, file);
-    // export
     let filename = file.split(".")[0];
     let ext = ".jpg";
     let exportFileName = filename + ext;
     let exportFilePath = path.join(exportPath, exportFileName);
     // rename file
     fs.renameSync(filePath, exportFilePath);
-    i++;
   });
 });
 
+// confirmation message
 console.log(`Conversion Complete!`);
